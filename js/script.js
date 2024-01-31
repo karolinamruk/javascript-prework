b4f37b897e5271b383418eec11c570bdb3613bf5
Karolina Mruk <kmruk88@gmail.com>
2024-01-31 17:20:42 +0100

    Changes in script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
-let computerMove = `kamień`;
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+​
+console.log('Gracz wpisał: ' + playerInput);
+​
+let playerMove = 'nieznany ruch';
+​
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+​if(playerInput == '2'){
+    playerMove = 'papier';
+  }
+if(playerInput == '3'){
+    playerMove = 'nożyce';
+  }
+printMessage('Twój ruch to: ' + playerMove);
 
-printMessage(
-  "Zagrałem " + computerMove + "! Jeśli Twój ruch to papier, to wygrywasz!"
-);
-
-let playerMove = `papier`;
-
-printMessage(
-  "Zagrałem " + playerMove + "! Jeśli Twój ruch to nożyce, to wygrywasz!"
-);
