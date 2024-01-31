2c836191f114f51619f16d59826aff13a919e5b4
Karolina Mruk <kmruk88@gmail.com>
2024-01-31 17:26:00 +0100

    Changes in script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-​
-console.log('Gracz wpisał: ' + playerInput);
-​
-let playerMove = 'nieznany ruch';
-​
-if(playerInput == '1'){
-  playerMove = 'kamień';
-}
-​if(playerInput == '2'){
-    playerMove = 'papier';
-  }
-if(playerInput == '3'){
-    playerMove = 'nożyce';
-  }
-printMessage('Twój ruch to: ' + playerMove);
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
+console.log("Wylosowana liczba to: " + randomNumber);
+
+let computerMove = "nieznany ruch";
+
+if (randomNumber == 1) {
+  computerMove = "kamień";
+}
+if (randomNumber == 2) {
+  computerMove = "papier";
+}
+if (randomNumber == 1) {
+  computerMove = "nożyce";
+}
+
+printMessage("Mój ruch to: " + computerMove);
